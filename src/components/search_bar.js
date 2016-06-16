d773b7e124002f58f3bb337e977ea85a3948c038
bugrauluyurt <bugra.u@gmail.com>
2016-06-16 19:35:57 +0300

    SearchBar Module Created

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 
 class Searchbar extends Component {
 	render() {
-		return <input />;
+		return <input onChange = {event => console.log(event.target.value)} />;
 	}
-}
+};
 
-export default SearchBar;
+export default Searchbar;
